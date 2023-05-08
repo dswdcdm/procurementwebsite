ced2c1b59d5f71753274250fd0385119619048f2
dswdcdm <dswdcdm@gmail.com>
2023-05-08 13:53:55 +0800

    udate

--- procurementwebsite/app/Views/pages/directory.php
+++ procurementwebsite/app/Views/pages/directory.php
@@ -3,136 +3,121 @@
 <div class="teamWrapper">
     <div class="section-container">
         <div class="topgrid">
-            <div class="colmun  pl-5 pr-5">
+            <div class="colmun pl-5 pr-5">
                 <div class="teamcol">
-                    <?php foreach ($employee as $employee) : ?>
-                        <?php if ($employee['id'] == 1) : ?>
-                            <div class="teamcolinner">
-                                <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " width="60%" height="60%" alt="logo" class="rounded-circle img-fluid" alt="avatar" style="width: 150px;" /></div>
-                                <div class="member-name">
-                                    <h2 align="center"><?= $employee['name']; ?>
-                                    </h2>
-                                </div>
-                                <div class="member-info">
-                                    <p align="center"><?= $employee['about']; ?></p>
-                                </div>
-                                <div class="member-mail">
-                                    <p align="center"> <a href="mailto:@gmail.com"><?= $employee['email']; ?></a> </p>
-                                </div>
-                                <div class="member-social">
-                                    <ul class="social-listing">
-                                        <li class="text-dark font-weight-bold"><a class="btn btn-primary" style="background-color: #3b5998;" href="https://www.facebook.com/" role="button" target="_blank"> <i class="fab fa-facebook-f"></i></a></li>
-                                        <li class="text-dark font-weight-bold"> <a class="btn btn-primary" style="background-color: #ac2bac;" href="#!" role="button" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                                        <li class="text-dark font-weight-bold"> <a class="btn btn-primary" style="background-color: #ed302f;" href="#!" role="button" target="_blank"><i class="fab fa-youtube"></i></a></li>
+                    <div class="teamcolinner">
+                        <div class="card-body">
+                            <div class="row">
+                                <?php foreach ($employee as $employee) : ?>
+                                    <?php if ($employee['id'] == 1) : ?>
+                                        <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0 ">
+                                            <div class="bg-image text-center hover-zoom ripple rounded ripple-surface">
+                                                <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class="rounded-circle img-fluid" style="width: 180px;" />
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-lg-6 col-xl-6">
+                                            <h5>I'M <?= $employee['name']; ?></h5>
+                                            <div class="d-flex flex-row">
+                                                <span class="font-weight-bold"><?= $employee['about']; ?></span>
+                                            </div>
+                                            <div class="mt-1 mb-0 text-muted small">
+                                                <span> <?= $employee['email']; ?></span>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                                            <div class="d-flex flex-row align-items-center mb-1">
+                                                <h4 class="mb-1 me-1"> <?= $employee['name']; ?></h4>
+                                            </div>
+                                            <?php if ($employee['status'] == "PERMANENT") : ?>
+                                                <h6 class="text-success"> <?= $employee['status']; ?> </h6>
+                                            <?php else : ?>
+                                                <h6 class="text-danger"> <?= $employee['status']; ?> </h6>
+                                            <?php endif; ?>
 
-                                    </ul>
-                                </div>
+                                            <div class="d-flex flex-column mt-4">
+                                                <a href="/Product/productItem/<?= $employee['id']; ?>" class="btn btn-primary btn-sm" type="button">Contact</a>
+
+                                            </div>
+                                        </div>
+                                    <?php else : ?>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
                             </div>
-                        <?php else : ?>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-        </div>
-        <div class="colmun mx-auto pl-5 pr-5">
-            <div class="teamcol">
-                <div class="teamcolinner">
-                    <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " width="60%" height="60%" alt="logo" class="rounded-circle img-fluid" alt="avatar" style="width: 150px;" /></div>
-                    <div class="member-name">
-                        <h2 align="center">Mohamed Yousef
-                        </h2>
-                    </div>
-                    <div class="member-info">
-                        <p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p>
-                    </div>
-                    <div class="member-mail">
-                        <p align="center"> <a href="mailto:@gmail.com">mailto:@gmail.com</a> </p>
-                    </div>
-                    <div class="member-social">
-                        <ul class="social-listing">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid  p-5">
-        <div class="row  justify-content-around gap-12">
-            <div class="col-12 col-lg-3 pl-5 pr-5">
-                <div class="teamcol">
-                    <div class="teamcolinner">
-                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " width="60%" height="60%" alt="logo" class="rounded-circle img-fluid" alt="avatar" style="width: 150px;" /></div>
-                        <div class="member-name">
-                            <h2 align="center">Mohamed Yousef
-                            </h2>
-                        </div>
-                        <div class="member-info">
-                            <p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p>
-                        </div>
-                        <div class="member-mail">
-                            <p align="center"> <a href="mailto:@gmail.com">mailto:@gmail.com</a> </p>
-                        </div>
-                        <div class="member-social">
-                            <ul class="social-listing">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-lg-3 pl-5 pr-5">
-                <div class="teamcol">
-                    <div class="teamcolinner">
-                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " width="60%" height="60%" alt="logo" class="rounded-circle img-fluid" alt="avatar" style="width: 150px;" /></div>
-                        <div class="member-name">
-                            <h2 align="center">Mohamed Yousef
-                            </h2>
-                        </div>
-                        <div class="member-info">
-                            <p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p>
-                        </div>
-                        <div class="member-mail">
-                            <p align="center"> <a href="mailto:@gmail.com">mailto:@gmail.com</a> </p>
-                        </div>
-                        <div class="member-social">
-                            <ul class="social-listing">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-lg-3 pl-5 pr-5">
-                <div class="teamcol">
-                    <div class="teamcolinner">
-                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " width="60%" height="60%" alt="logo" class="rounded-circle img-fluid" alt="avatar" style="width: 150px;" /></div>
-                        <div class="member-name">
-                            <h2 align="center">Mohamed Yousef
-                            </h2>
-                        </div>
-                        <div class="member-info">
-                            <p align="center">Lorem Ipsum is a simply dummy text of the printing and typesetting industry.</p>
-                        </div>
-                        <div class="member-mail">
-                            <p align="center"> <a href="mailto:@gmail.com">mailto:@gmail.com</a> </p>
-                        </div>
-                        <div class="member-social">
-                            <ul class="social-listing">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="col-6 offset-md-3 offset-2">
+                <div class="teamcol">
+                    <div class="teamcolinner">
+                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class="img-fluid"  /></div>
+                        <div class="member-name">
+                            <h2 style="font-size:1rem;"><?= $employee['name']; ?></h2>
+                            <p style="font-size:0.8em;"><?= $employee['about']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <div class="container">
+        <div class="row justify-content-md-between ">
+            <div class="col-12 col-lg-3">
+                <div class="teamcol">
+                    <div class="teamcolinner">
+                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class=" img-fluid" /></div>
+                        <div class="member-name">
+                            <h2 style="font-size:1rem;"><?= $employee['name']; ?></h2>
+                            <p style="font-size:0.8em;"><?= $employee['about']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+                <div class="teamcol">
+                    <div class="teamcolinner">
+                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class=" img-fluid" /></div>
+                        <div class="member-name">
+                            <h2 style="font-size:1rem;"><?= $employee['name']; ?></h2>
+                            <p style="font-size:0.8em;"><?= $employee['about']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+                <div class="teamcol">
+                    <div class="teamcolinner">
+                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class=" img-fluid" /></div>
+                        <div class="member-name">
+                            <h2 style="font-size:1rem;"><?= $employee['name']; ?></h2>
+                            <p style="font-size:0.8em;"><?= $employee['about']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-md-evenly gap-4 ">
+            <div class="col-12 col-lg-3">
+                <div class="teamcol">
+                    <div class="teamcolinner">
+                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class=" img-fluid" /></div>
+                        <div class="member-name">
+                            <h2 style="font-size:1rem;"><?= $employee['name']; ?></h2>
+                            <p style="font-size:0.8em;"><?= $employee['about']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+                <div class="teamcol">
+                    <div class="teamcolinner">
+                        <div class="avatar"> <img src="<?php echo base_url('assets/images/pic.jpg'); ?> " alt="profile" class="img-fluid"  /></div>
+                        <div class="member-name">
+                            <h2 style="font-size:1rem;"><?= $employee['name']; ?></h2>
+                            <p style="font-size:0.8em;"><?= $employee['about']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
