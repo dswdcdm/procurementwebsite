59edc52e9a8903fc748afc1b44a900ef132c791c
dswdcdm <dswdcdm@gmail.com>
2023-05-10 16:19:03 +0800

    da

--- procurementwebsite/app/Views/pages/addProduct.php
+++ procurementwebsite/app/Views/pages/addProduct.php
@@ -8,59 +8,18 @@
     </nav>
 </div>
 <div class="blog-single gray-bg">
-<<<<<<< HEAD
-    <form action="<?php echo htmlspecialchars(site_url('Product/saveProduct'));?>" method="POST" autocomplete="off">
-        <?= csrf_field(); ?>
-        <?php if (!empty(session()->getFlashdata('fail'))) : ?>
-            <div class="alert alert-danger"><?= session()->getFlashdata('fail'); ?></div>
-        <?php endif ?>
-        <?php if (!empty(session()->getFlashdata('success'))) : ?>
-            <div class="alert alert-success"><?= session()->getFlashdata('success'); ?></div>
-        <?php endif ?>
-        <div class="container">
-            <div class="row align-items-start">
-                <div class="col-lg-8 m-15px-tb">
-                    <article class="article">
-                        <div class="article-img">
-                            <img src="<?php echo base_url('assets/images/logob.png   '); ?>" width="50%" height="60%" alt="logo" />
-                        </div>
-                        <div class="article-title">
-                            <h6>
-                                <a href="#">Product Overview</a>
-                                <h6 class="text-dark">
-                                    <label for="name">PRODUCT NAME</label>
-                                    <div class="input_container">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" fill="currentColor" class="bi bi-archive icon text-success" viewBox="0 0 16 16">
-                                            <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
-                                        </svg>
-                                        <input placeholder="PRODUCT NAME" name="name" type="text" class="input_field" id="name" value="" required>
-                                        <span class="text-danger"><?= isset($validation) ? display_error($validation, 'name') : '' ?></span>
-                                    </div>
-                                </h6>
-                            </h6>
-                            <h6>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label for="name">PRODUCT DESCRIPTION</label>
-                                        <div class="form-group">
-                                            <textarea name="description" id="description" placeholder="Product Description*" rows="4" class="form-control" value="" required></textarea>
-                                            <span class="text-danger"><?= isset($validation) ? display_error($validation, 'description') : '' ?></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </h6>
-=======
+
 
     <div class="container">
         <div class="row align-items-start">
             <div class="col-lg-8 m-15px-tb">
                 <article class="article">
-                   
+
                     <div class="article-title">
                         <h6>
-                       <!--Image-->
-                       <label for="formFileSm" class="form-label">SELECT IMAGE</label>
-<input class="form-control form-control-sm" id="formFileSm" type="file" />
+                            <!--Image-->
+                            <label for="formFileSm" class="form-label">SELECT IMAGE</label>
+                            <input class="form-control form-control-sm" id="formFileSm" type="file" />
                             <h6 class="text-dark">
                                 <label for="name">PRODUCT NAME</label>
                                 <div class="input_container">
@@ -84,27 +43,27 @@
                         <div class="media-body">
                             <label>
                                 <h6>
-                                <label for="name" class = "text-dark">PRODUCT PRICE</label>
-                                <div class="input_container">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" fill="currentColor" class="bi bi-tags-fill icon" viewBox="0 0 16 16">
-                                        <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                                        <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
-                                    </svg>
-                                    <input placeholder="PRODUCT PRICE" name="productprice" type="number" class="input_field" id="productprice" value="">
+                                    <label for="name" class="text-dark">PRODUCT PRICE</label>
+                                    <div class="input_container">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" fill="currentColor" class="bi bi-tags-fill icon" viewBox="0 0 16 16">
+                                            <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                                            <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
+                                        </svg>
+                                        <input placeholder="PRODUCT PRICE" name="productprice" type="number" class="input_field" id="productprice" value="">
 
-                                </label>
+                            </label>
                         </div>
                     </div>
                     <div class="article-content">
-                        <h6 class ="text-dark">
-                        <label for="name" class="text-dark">PRODUCT STATUS</label>
-                      
-                        <div class="input_container">
+                        <h6 class="text-dark">
+                            <label for="name" class="text-dark">PRODUCT STATUS</label>
 
-                            <input placeholder="PRODUCT STATUS" name="productstatus" type="text" class="input_field" id="productstatus" value=""></p>
-                        </div>
+                            <div class="input_container">
+
+                                <input placeholder="PRODUCT STATUS" name="productstatus" type="text" class="input_field" id="productstatus" value=""></p>
+                            </div>
                         </h6>
-</h6>
+                        </h6>
 
                         <div class="send">
 
@@ -124,146 +83,21 @@
                             <div class="item-avatar">
                                 <img src="<?php echo base_url('assets/images/logob.png'); ?>" width="100%" height="60%" alt="logo" />
                             </div>
->>>>>>> 69e470b9953673e465ed2bc2a13554940f8b213c
-                            <div class="media-body">
-                                <label>
-                                    <label for="price">PRODUCT PRICE</label>
-                                    <div class="input_container">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="15" fill="currentColor" class="bi bi-tags-fill icon" viewBox="0 0 16 16">
-                                            <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                                            <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z" />
-                                        </svg>
-                                        <input placeholder="PRODUCT PRICE" name="price" type="number" class="input_field" id="price" value="" required>
-                                        <span class="text-danger"><?= isset($validation) ? display_error($validation, 'price') : '' ?></span>
-                                </label>
-                            </div>
                         </div>
                         <div class="article-content">
                             <label for="status">PRODUCT STATUS</label>
-                            <div class="input_container">
-                                <input placeholder="PRODUCT STATUS" name="status" type="text" class="input_field" id="status" value="" required></p>
-                                <span class="text-danger"><?= isset($validation) ? display_error($validation, 'status') : '' ?></span>
-                            </div>
-
                             <div class="send">
                                 <button type="submit" class="px-btn theme w-100 btn btn-primary"><span>ADD ITEM</span> <i class="arrow"></i></button>
                             </div>
                         </div>
-                    </article>
-                </div>
-                <div class="col-lg-4 m-15px-tb blog-aside">
-                    <!-- Author -->
-                    <div class="widget widget-author">
-                        <div class="widget-title">
-                            <h3>NOTE</h3>
-                        </div>
-                        <div class="widget-body">
-                            <div class="media align-items-center">
-                                <div class="item-avatar">
-                                    <img src="<?php echo base_url('assets/images/logob.png'); ?>" width="100%" height="60%" alt="logo" />
-                                </div>
-                                <div class="media-body">
-                                    <h6>Hello, I'm<br>admin</h6>
-                                </div>
-                            </div>
-                            <p>
-                            <div class="form-group">
-                                <textarea name="message" id="message" placeholder="Note" rows="4" class="form-control"></textarea>
-                            </div>
-                            </p>
-                        </div>
                     </div>
-<<<<<<< HEAD
-                    <div class="widget widget-tags">
-                        <div class="widget-title">
-                            <h3>DOCUMENTS</h3>
-                        </div>
-                        <div class="widget-body">
-                            <div class="nav tag-cloud">
-                                <a href="" data-toggle="modal" data-target="#downloadModal">UPLOAD TS</a>
-                                <a href="" data-toggle="modal" data-target="#viewModal">UPLOAD MS</a>
-                                <!-- Modal -->
-                                <div class="modal fade" id="downloadModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-=======
-                    <div class="widget-body">
-                        <div class="nav tag-cloud">
-                        <label for="formFileSm" class="form-label"><H6>UPLOAD TS</H6></label>
-<input class="form-control form-control-sm" id="formFileSm" type="file" />
-<label for="formFileSm" class="form-label mt-2"><H6>UPLOAD MS</H6></label>
-<input class="form-control form-control-sm" id="formFileSm" type="file" />
-                            <!-- <a href="" data-toggle="modal" data-target="#downloadModal">UPLOAD TS</a>
-                            <a href="" data-toggle="modal" data-target="#viewModal">UPLOAD MS</a> -->
-                            <!-- Modal -->
-                            <div class="modal fade" id="downloadModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
->>>>>>> 69e470b9953673e465ed2bc2a13554940f8b213c
 
-                                            <div class="modal-footer">
-                                                <a href="" target="_blank" class="btn btn-secondary">DOWNLOAD SPECIFICATION TA</a>
-                                                <a href="" target="_blank" class="btn btn-secondary">DOWNLOAD MARKET SCANNING</a>
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">CANCEL</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Modal -->
-                                <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-
-                                            <div class="modal-footer">
-                                                <a href="" target="_blank" class="btn btn-secondary">VIEW SPECIFICATION TA</a>
-                                                <a href="" target="_blank" class="btn btn-secondary">VIEW MARKET SCANNING</a>
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">CANCEL</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                PRODUCT SPECS IS IN ACTIVE
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
+
+
+
+
         </div>
-    </form>
+    </div>
 </div>
