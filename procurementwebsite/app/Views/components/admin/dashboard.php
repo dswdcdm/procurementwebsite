ef089b03eb796cc65eed1427400883d406a2cc90
dswdcdm <dswdcdm@gmail.com>
2023-05-10 13:08:08 +0800

    update

--- procurementwebsite/app/Views/components/admin/dashboard.php
+++ procurementwebsite/app/Views/components/admin/dashboard.php
@@ -1,6 +1,6 @@
 <div class="content">
   <div class="container-fluid">
-    <div class="row">
+    <div class="row mb-5">
       <div class="col-xl-4 col-lg-12">
         <div class="card card-chart">
           <div class="card-header card-header-success">
@@ -8,7 +8,7 @@
           </div>
           <div class="card-body">
             <h4 class="card-title">DOWNLOADS</h4>
-            <p class="card-category">
+            <p class="card-category d-flex gap-4">
               <?php foreach ($downloads as $download) : ?>
                 <?php if ($download['id'] == 1) : ?>
                   <span class="text-success"><i class="fa fa-download"></i> <?= $download['download_number']; ?></span> TA DOWNLOADS <br />
@@ -51,75 +51,7 @@
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-          <div class="card-header card-header-warning card-header-icon">
-            <div class="card-icon">
-              <i class="material-icons">content_copy</i>
-            </div>
-            <p class="card-category">Used Space</p>
-            <h3 class="card-title">49/50
-              <small>GB</small>
-            </h3>
-          </div>
-          <div class="card-footer">
-            <div class="stats">
-              <i class="material-icons text-warning">warning</i>
-              <a href="#pablo" class="warning-link">Get More Space...</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-          <div class="card-header card-header-success card-header-icon">
-            <div class="card-icon">
-              <i class="material-icons">store</i>
-            </div>
-            <p class="card-category">Revenue</p>
-            <h3 class="card-title">$34,245</h3>
-          </div>
-          <div class="card-footer">
-            <div class="stats">
-              <i class="material-icons">date_range</i> Last 24 Hours
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-          <div class="card-header card-header-danger card-header-icon">
-            <div class="card-icon">
-              <i class="material-icons">info_outline</i>
-            </div>
-            <p class="card-category">Fixed Issues</p>
-            <h3 class="card-title">75</h3>
-          </div>
-          <div class="card-footer">
-            <div class="stats">
-              <i class="material-icons">local_offer</i> Tracked from Github
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
-        <div class="card card-stats">
-          <div class="card-header card-header-info card-header-icon">
-            <div class="card-icon">
-              <i class="fa fa-twitter"></i>
-            </div>
-            <p class="card-category">Followers</p>
-            <h3 class="card-title">+245</h3>
-          </div>
-          <div class="card-footer">
-            <div class="stats">
-              <i class="material-icons">update</i> Just Updated
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    
     <div class="row">
       <div class="col-lg-6 col-md-12">
         <div class="card">
