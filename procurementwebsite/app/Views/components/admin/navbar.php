be2801e76f6392823d164159dbf78c036de4bfba
dswdcdm <dswdcdm@gmail.com>
2023-05-11 09:29:54 +0800

    waratan

--- procurementwebsite/app/Views/components/admin/navbar.php
+++ procurementwebsite/app/Views/components/admin/navbar.php
@@ -13,8 +13,7 @@
             <li class="menu-item hidden"><a href="/admin">HOME</a></li>
             <li class="menu-item hidden"><a href="<?= site_url('/admin/adminproduct')  ?>">PRODUCTS</a>
             </li>
-            <li class="menu-item hidden"><a href="<?= site_url('/admin/userProfile')  ?>">USERS</a>
-            </li>
+           
             <li class="menu-item hidden"><a href="<?= site_url('auth/logout') ?>">LOG OUT</a>
             </li>
         </ul>
