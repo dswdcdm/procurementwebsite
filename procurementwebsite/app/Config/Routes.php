4840ba4f3def652aa7fbd8a58db149bbd39941de
dswdcdm <dswdcdm@gmail.com>
2023-05-11 11:07:44 +0800

    update viewusers

--- procurementwebsite/app/Config/Routes.php
+++ procurementwebsite/app/Config/Routes.php
@@ -52,6 +52,7 @@ $routes->group('', ['filter' => 'AuthCheck'], function ($routes) {
     $routes->add('admin/adminproduct', 'Admin::adminproduct');
     $routes->add('admin/addproduct', 'Admin::adminaddproduct');
     $routes->add('admin/updateproduct', 'Admin::adminupdateproduct');
+    $routes->add('admin/ViewUser', 'Admin::viewusers');
 });
 
 $routes->group('', ['filter' => 'AlreadyLoggedInFilter'], function ($routes) {
