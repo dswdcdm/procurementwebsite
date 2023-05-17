949fb229d1897f9fc6e259ee343b4c7434a44960
dswdcdm <dswdcdm@gmail.com>
2023-05-17 09:44:20 +0800

    agsfsdgdfgdf

--- procurementwebsite/app/Views/pages/product.php
+++ procurementwebsite/app/Views/pages/product.php
@@ -30,19 +30,30 @@
                     <div class="col-md-6 col-lg-6 col-xl-6">
                         <h5><?= $product['name']; ?></h5>
                         <div class="d-flex flex-row">
+
                             <div class="text-danger mb-1 me-2">
                                 <?php
                                 /*  for ($i = 1; $i <= $product['ratings']; $i++) { */
-                                echo '  <i class="fa fa-download"></i> DOWNLOADS';
+                                echo '  <i class="fa fa-download"></i> TS DOWNLOADS';
                                 /*   } */
                                 ?>
                             </div>
-                            <span class="font-weight-bold"><?= $product['ratings']; ?></span>
+                            <span class="font-weight-bold"><?= $product['downloadts']; ?> &nbsp; </span>
+                            <div class="text-danger mb-1 me-2">
+                                <?php
+                                /*  for ($i = 1; $i <= $product['ratings']; $i++) { */
+                                echo '  <i class="fa fa-download"></i> MS DOWNLOADS';
+                                /*   } */
+                                ?>
+                            </div>
+                            <span class="font-weight-bold"><?= $product['downloadms']; ?></span>
                         </div>
                         <div class="mt-1 mb-0 text-muted small">
                             <span> <?= $product['description']; ?></span>
                         </div>
                     </div>
+
+
                     <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
                         <div class="d-flex flex-row align-items-center mb-1">
                             <h4 class="mb-1 me-1">₱ <?= $product['price']; ?></h4>
