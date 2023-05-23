f91f2c35706a8227e367208694d223317c4da9be
dswdcdm <dswdcdm@gmail.com>
2023-05-23 08:35:01 +0800

    erttre

--- procurementwebsite/app/Controllers/Home.php
+++ procurementwebsite/app/Controllers/Home.php
@@ -4,8 +4,16 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    public function __construct()
+    {
+        helper(['url', 'form']);
+    }
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'title' => 'login'
+        ];
+        return view('components/header', $data)
+            . view('auth/login');
     }
 }
