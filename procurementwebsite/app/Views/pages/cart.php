ed4b8728301d16f5d3f74ca26fa4b0eb867e65fa
dswdcdm <dswdcdm@gmail.com>
2023-05-29 09:19:13 +0800

    updated

--- procurementwebsite/app/Views/pages/cart.php
+++ procurementwebsite/app/Views/pages/cart.php
@@ -1,7 +1,7 @@
 <section style="background-color: #eee;">
     <div class="container py-5">
 
-<div class="row">
+        <div class="row">
             <div class="col">
                 <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
                     <ol class="breadcrumb mb-0">
@@ -24,62 +24,79 @@
             <div class="cart text-decoration-none mt-2" id="">
                 <div class=" menu shadow  w-100">
                     <?php foreach ($cartData as $cartitem) : ?>
-                        <?php if ($cartitem['user_id'] == $userInfo['id']) : ?>
-                            <div class="col-12 col-md-12 mt-4 ">
-                                <div class="card shadow-0 border rounded-3">
-                                    <div class="card-body">
-                                        <div class="row">
+                    <?php if ($cartitem['user_id'] == $userInfo['id']) : ?>
+                    <div class="col-12 col-md-12 mt-4 ">
+                        <div class="card shadow-0 border rounded-3">
+                            <div class="card-body">
+                                <div class="row">
 
-                                            <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0 ">
-                                                <div class="bg-image hover-zoom ripple rounded ripple-surface">
-                                                    <img src="<?= $cartitem['item_image']; ?>" width="50%" height="60%" alt="logo" />
-                                                    <img src="<?php echo base_url('assets/images/checked.png'); ?>" width="20%" height="20%" alt="logo" />
-                                                </div>
-                                            </div>
+                                    <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0 ">
+                                        <div class="bg-image hover-zoom ripple rounded ripple-surface">
+                                            <img src="<?= $cartitem['item_image']; ?>" width="50%" height="60%"
+                                                alt="logo" />
+                                            <img src="<?php echo base_url('assets/images/checked.png'); ?>" width="20%"
+                                                height="20%" alt="logo" />
+                                        </div>
+                                    </div>
 
-                                            <div class="col-md-6 col-lg-6 col-xl-6">
-                                                <div class="d-flex flex-row">
-                                                    <span class="font-weight-bold"> <?= $cartitem['item_name']; ?> </span>
-                                                </div>
-                                                <div class="mt-1 mb-0 text-muted small">
-                                                    <span><?= $cartitem['date_created']; ?> <br /><?= $cartitem['item_description']; ?> </span>
-                                                </div>
-                                            </div>
+                                    <div class="col-md-6 col-lg-6 col-xl-6">
+                                        <div class="d-flex flex-row">
+                                            <span class="font-weight-bold"> <?= $cartitem['item_name']; ?> </span>
+                                        </div>
+                                        <div class="mt-1 mb-0 text-muted small">
+                                            <span><?= $cartitem['date_created']; ?>
+                                                <br /><?= $cartitem['item_description']; ?> </span>
+                                        </div>
+                                    </div>
 
-                                            <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
-                                                <div class="d-flex flex-row align-items-center mb-1">
+                                    <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                                        <div class="d-flex flex-row align-items-center mb-1">
 
-                                                    <!--      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                            <!--      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                                                         <?= $cartitem['quantity']; ?>
                                                     </span> -->
-                                                    <h4 class="mb-1 me-1 ">₱ <?= $cartitem['item_price']; ?></h4>
+                                            <h4 class="mb-1 me-1 ">₱ <?= $cartitem['item_price']; ?></h4>
 
+                                        </div>
+
+                                        <form method="POST"
+                                            action="<?= site_url('itemcart/delete/' . $cartitem['id']) ?>">
+                                            <div class="d-flex flex-column mt-4">
+                                                <button type="submit" class="btn btn-danger btn-sm">DELETE</button>
+
+                                            </div>
+                                        </form>
+                                        <span class="mb-1 me-1 mt-2">
+                                            <!-- Quantity -->
+                                            <div class="d-flex mb-4 mt-2" style="max-width: 100%">
+
+                                                <form method="POST" action="<?php echo htmlspecialchars(base_url('cart/deleteItem/') . $cartitem['item_id'] . "/" . $cartitem['user_id']); ?>" autocomplete="off">
+                                                    <button class="btn btn-primary btn-sm px-2 me-3" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                                        <i class="fas fa-minus"></i>
+                                                    </button>
+                                                </form>
+
+                                                <div class="form-outline">
+                                                    <input class="text-center ml-4" type="number" style="width: 40px;" placeholder="<?= $cartitem['quantity']; ?>" value="<?= $cartitem['quantity']; ?>"  required />
+                                                    <label class="form-label ml-3" for="form1">Quantity</label>
                                                 </div>
 
-                                                <form method="POST" action="<?= site_url('itemcart/delete/' . $cartitem['id']) ?>">
-                                                    <div class="d-flex flex-column mt-4">
-                                                        <button type="submit" class="btn btn-danger btn-sm">DELETE</button>
-
-                                                    </div>
+                                                <form method="POST" action="<?php echo htmlspecialchars(base_url('cart/addItem/') . $cartitem['item_id']."/".$cartitem['user_id']); ?>" autocomplete="off">
+                                                    <button class="btn btn-primary btn-sm px-2 ms-3" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                                        <i class="fas fa-plus"></i>
+                                                    </button>
                                                 </form>
-                                                <span class="mb-1 me-1 mt-2 mx-auto">
-                                                    <form method="POST" action="<?php echo htmlspecialchars(base_url('cart/deleteItem/') . $cartitem['item_id'] . "/" . $cartitem['user_id']); ?>" autocomplete="off">
-                                                        <button class="btn btn-primary btn-sm " type="submit">-</button>
-                                                    </form>
-                                                    
-                                                    <input type="number" style="width: 40px;" placeholder="<?= $cartitem['quantity']; ?>" value="<?= $cartitem['quantity']; ?>" class="text-center " required />
-                                                    
-                                                    <form method="POST" action="<?php echo htmlspecialchars(base_url('cart/addItem/') . $cartitem['item_id']."/".$cartitem['user_id']); ?>" autocomplete="off">
-                                                        <button type="submit" class="btn btn-primary btn-sm ">+</button>
-                                                    </form>
-                                                </span>
+
                                             </div>
-                                        </div>
+                                            <!-- Quantity -->
+                                        </span>
                                     </div>
                                 </div>
                             </div>
-                        <?php else : ?>
-                        <?php endif; ?>
+                        </div>
+                    </div>
+                    <?php else : ?>
+                    <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
 
@@ -97,7 +114,7 @@
                 </div>
             </div>
 
-          
-       
+
+
 
 </section>
